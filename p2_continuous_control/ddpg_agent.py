837b8dabe4d32fc3b7767713591fe76199cf9f72
Melan <melan2308@gmail.com>
2018-10-20 22:15:50 +0200

    Add class for Ornstein-Uhlenbeck (OU) noise to improve exploration

--- p2_continuous_control/ddpg_agent.py
+++ p2_continuous_control/ddpg_agent.py
@@ -131,3 +131,30 @@ class Agent():
 		for target_pararam, local_param in zip(target_model.parameters(), local_model.parameters()):
 			target_param.data.copy_(tau*local_param.data + (1.0 - tau)*target_param.data)
 
+
+
+class OUNoise():
+	""" Ornstein-Uhlenbeck process """
+
+	def __init__(self, size, mu=0.0, theta=0.15, sigma=0.2):
+		""" Initialize parameters and noise process """
+		self.mu = mu * np.ones(size)
+		self.theta = theta 
+		self.sigma = sigma 
+		self.reset()
+
+	def reset(self):
+		""" Reset the interal state (= noise) to mean (mu). """
+		self.state = copy.copy(self.mu)
+
+	def sample(self):
+		""" Update internal state and return it as a noise sample """
+		x = self.state 
+		dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+		self.state = x + dx 
+
+		return self.state
+
+
+
+	
